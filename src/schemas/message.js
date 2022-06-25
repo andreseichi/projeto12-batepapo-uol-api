b050d74ac48313a69bdad0be6954b869be059a57
andreseichi <andre_seichi@hotmail.com>
2022-06-25 12:11:47 -0300

    message schema

---
+++ src/schemas/message.js
@@ -0,0 +1,8 @@
+import joi from 'joi';
+
+export const messageSchema = joi.object({
+  to: joi.string().required(),
+  text: joi.string().required(),
+  type: joi.string(),
+  from: joi.string().required(),
+});

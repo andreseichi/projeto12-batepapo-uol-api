97f66d28b623b24916b56b0c1279c56ac70057f6
andreseichi <andre_seichi@hotmail.com>
2022-06-27 13:13:27 -0300

    trim no text

--- src/schemas/message.js
+++ src/schemas/message.js
@@ -2,7 +2,7 @@ import joi from 'joi';
 
 export const messageSchema = joi.object({
   to: joi.string().required(),
-  text: joi.string().required(),
+  text: joi.string().trim().required(),
   type: joi.string().valid('message', 'private_message'),
   from: joi.string().required(),
 });
